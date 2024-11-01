35cb09cacf07509bfd9fe21be3b3d61ed09f1317
Yoanna Evgenieva <yoanna.evgenieva@nuvei.com>
2024-11-01 13:11:42 +0000

    Merged PR 99659: Change Safecharge to Nuvei reference
    Change Safecharge to Nuvei reference
    
    Related work items: #437452

---
+++ src/main/java/com/nuvei/model/AddendumsAirlines.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2007 - 2024 Nuvei International Group Limited.
+ */
+
+package com.nuvei.model;
+
+import javax.validation.Valid;
+
+/**
+ * <p>
+ * Local payment info specific for some clients. Part of {@link Addendums}.
+ *
+ * @author <a mailto:bozhidarsh@nuvei.com>Bozhidar Shumanov</a>
+ * @see Addendums
+ * @since 8/8/2018
+ */
+public class AddendumsAirlines {
+
+    @Valid
+    private AddendumsAirlinesReservationDetails reservationDetails;
+
+    public AddendumsAirlinesReservationDetails getReservationDetails() {
+        return reservationDetails;
+    }
+
+    public void setReservationDetails(AddendumsAirlinesReservationDetails reservationDetails) {
+        this.reservationDetails = reservationDetails;
+    }
+}
---
+++ src/main/java/com/nuvei/model/PaymentDetailsThreeD.java
@@ -0,0 +1,20 @@
+
+
+/*
+ * Copyright (C) 2007 - 2024 Nuvei International Group Limited.
+ */
+
+package com.nuvei.model;
+
+public class PaymentDetailsThreeD {
+
+    private String isLiabilityOnIssuer;
+
+    public String getIsLiabilityOnIssuer() {
+        return isLiabilityOnIssuer;
+    }
+
+    public void setIsLiabilityOnIssuer(String isLiabilityOnIssuer) {
+        this.isLiabilityOnIssuer = isLiabilityOnIssuer;
+    }
+}
